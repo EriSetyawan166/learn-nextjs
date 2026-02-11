f8f3347f38f82167ada58b4ccffa75f9417b369d
erisetyawan166 <erisetyawan34@gmail.com>
2026-02-11 22:48:56 +0700

    Section 4: Routing & Page Rendering - Deep Dive

---
+++ Routing and Page Rendering - Deep Dive/app/(content)/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <div id="error">
+            <h1>Not Found</h1>
+            <p>Sorry, but we could not find the requested page or news item.</p>
+        </div>
+    )
+}
